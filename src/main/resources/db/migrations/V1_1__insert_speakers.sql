1cd018401e17890585ab110f9cb62c90f65118e6
Scott Morgan <smorgan@cardinalsolutions.com>
2018-02-26 11:05:40 -0500

    Added insert script

---
+++ src/main/resources/db/migrations/V1_1__insert_speakers.sql
@@ -0,0 +1,19 @@
+INSERT INTO public.speaker
+(firstname, lastname, bio, first_name, last_name)
+VALUES('Michelle', 'Morgan', NULL, NULL, NULL);
+
+INSERT INTO public.speaker
+(firstname, lastname, bio, first_name, last_name)
+VALUES('Rowan', 'Morgan', NULL, NULL, NULL);
+
+INSERT INTO public.speaker
+(firstname, lastname, bio, first_name, last_name)
+VALUES('Georgia', 'Morgan', NULL, NULL, NULL);
+
+INSERT INTO public.speaker
+(firstname, lastname, bio, first_name, last_name)
+VALUES('Aaron', 'Morgan', NULL, NULL, NULL);
+
+INSERT INTO public.speaker
+(firstname, lastname, bio, first_name, last_name)
+VALUES('Scott', 'Morgan', NULL, NULL, NULL);
