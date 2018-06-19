6911150c68671252e2d745e9e29276d35fedb97a
Scott Morgan <smorgan@cardinalsolutions.com>
2018-06-19 08:43:29 -0400

    Changed namespace..again

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,7 +2,7 @@
 
 node('maven') {
 
-	def ocdevnamespace = "lexis-nexis-services"
+	def ocdevnamespace = "lexis-conference-services"
 	
 	def ocprodnamespace = "srm-parks-prod"
 	def appname = "spring-boot-demo";
@@ -19,11 +19,6 @@ node('maven') {
 	def version    = getVersionFromPom("pom.xml")
 	def newTag = "TestReady-${version}:${BUILD_NUMBER}"
 
-//	stage('Flyway Migration') {
-//		def sqlFilesDir = "filesystem:${WORKSPACE}/src/main/resources/db/migrations"
-//		flywayrunner commandLineArgs: '-baselineOnMigrate=true', credentialsId: 'ConferenceDB', flywayCommand: 'clean', installationName: 'flyway', locations: sqlFilesDir, url: 'jdbc:postgresql://postgresql.srm-conference-services.svc/conferencedb'
-//		flywayrunner commandLineArgs: '-baselineOnMigrate=true', credentialsId: 'ConferenceDB', flywayCommand: 'migrate', installationName: 'flyway', locations: sqlFilesDir, url: 'jdbc:postgresql://postgresql.srm-conference-services.svc/conferencedb'
-//	}
 
 
 	stage('Build war') {
@@ -63,48 +58,6 @@ node('maven') {
 		openshiftDeploy depCfg: appname, namespace: ocdevnamespace, verbose: 'false', waitTime: '', waitUnit: 'sec'
 		openshiftVerifyDeployment depCfg: appname, namespace: ocdevnamespace, replicaCount: '1', verbose: 'false', verifyReplicaCount: 'false', waitTime: '', waitUnit: 'sec'
 	}
-
-	//   newTag = "ProdReady-${version}:${BUILD_NUMBER}"
-	//   stage('Integration Test') {
-	//     echo "New Tag: ${newTag}"
-	//     openshiftTag alias: 'false', destStream: appname, destTag: newTag, destinationNamespace: ocdevnamespace, namespace: ocdevnamespace, srcStream: appname, srcTag: 'latest', verbose: 'false'
-	//   }
-
-	//   // Blue/Green Deployment into Production
-	//   // -------------------------------------
-	//   def dest   = "${appname}-green"
-	//   def active = ""
-
-	//   stage('Prepare Blue/Green Switch') {
-	//     sh "oc project ${ocprodnamespace}"
-	//     sh "oc get route ${appname} -n ${ocprodnamespace} -o jsonpath='{ .spec.to.name }' > activesvc.txt"
-	//     active = readFile('activesvc.txt').trim()
-	//     if (active == "${appname}-green") {
-	//       dest = "${appname}-blue"
-	//     }
-	//     echo "Active svc: " + active
-	//     echo "Dest svc:   " + dest
-	//   }
-
-	//  stage('Deploy To Prod') {
-	//     echo "Deploying to ${dest}"
-	//     sh "oc patch dc ${dest} --patch '{\"spec\": { \"triggers\": [ { \"type\": \"ImageChange\", \"imageChangeParams\": { \"containerNames\": [ \"$dest\" ], \"from\": { \"kind\": \"ImageStreamTag\", \"namespace\": \"$ocdevnamespace\", \"name\": \"$appname:$newTag\"}}}]}}' -n $ocprodnamespace"
-	//     openshiftDeploy depCfg: dest, namespace: ocprodnamespace, verbose: 'false', waitTime: '', waitUnit: 'sec'
-	//     openshiftVerifyDeployment depCfg: dest, namespace: ocprodnamespace, replicaCount: '1', verbose: 'false', verifyReplicaCount: 'true', waitTime: '', waitUnit: 'sec'
-	//     openshiftVerifyService namespace: ocprodnamespace, svcName: dest, verbose: 'false'
-	//   }
-
-	//   stage('Switch over to new Version') {
-	//     input "Switch NationaParks to Production?"
-
-	//     sh "oc patch route ${appname} -n ${ocprodnamespace} -p '{\"spec\":{\"to\":{\"name\":\"" + dest + "\"}}}'"
-
-	//     sh "oc get route ${appname} -n ${ocprodnamespace} > oc_out.txt"
-	//     oc_out = readFile('oc_out.txt')
-	//     echo "Current route configuration: " + oc_out
-	//   }
-
-
 
 }
 
